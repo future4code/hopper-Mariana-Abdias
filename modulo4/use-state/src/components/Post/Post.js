f939f540386b87e01906f1c101c6039f8d159a18
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-05-31 19:36:23 -0300

    Exercício Finalizado

--- modulo4/use-state/src/components/Post/Post.js
+++ modulo4/use-state/src/components/Post/Post.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
-import { PostContainer, PostHeader, UserPhoto, PostPhoto, PostFooter, CommentContainer } from './styles'
+import {
+  PostContainer,
+  PostHeader,
+  UserPhoto,
+  PostPhoto,
+  PostFooter,
+  CommentContainer
+} from './styles'
 
 import IconeComContador from '../IconeComContador/IconeComContador'
 import SecaoComentario from '../SecaoComentario/SecaoComentario'
@@ -8,41 +15,72 @@ import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 
-const Post = (props) => {
-
+const Post = props => {
+  const [curtido, setValorCurtido] = useState(false)
+  const [numeroCurtidas, setValorNumeroCurtidas] = useState(0)
+  const [comentando, setValorComentando] = useState(false)
+  const [numeroComentarios, setValorNumeroComentarios] = useState(0)
+  const [comentarios, setValorComentarios] = useState([])
 
   const onClickCurtida = () => {
-  };
+    if (curtido) {
+      setValorCurtido(!curtido)
+      setValorNumeroCurtidas(numeroCurtidas - 1)
+    } else {
+      setValorCurtido(!curtido)
+      setValorNumeroCurtidas(numeroCurtidas + 1)
+    }
+  }
 
   const onClickComentario = () => {
-  };
-
-  const enviarComentario = (comentario) => {
+    setValorComentando(!comentando)
   }
+
+  const enviarComentario = comentario => {
+    const listaDeComentarios = [...comentarios, comentario]
+
+    setValorComentarios(listaDeComentarios)
+    setValorComentando(false)
+    setValorNumeroComentarios(numeroComentarios + 1)
+  }
+
+  const iconeCurtida = curtido ? iconeCoracaoPreto : iconeCoracaoBranco
+
+  const caixaDeComentario = comentando ? (
+    <SecaoComentario enviarComentario={enviarComentario} />
+  ) : (
+    comentarios.map(comentario => {
+      return (
+        <CommentContainer>
+          <p>{comentario}</p>
+        </CommentContainer>
+      )
+    })
+  )
 
   return (
     <PostContainer>
       <PostHeader>
-        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'} />
         <p>{props.nomeUsuario}</p>
       </PostHeader>
 
-      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'} />
 
       <PostFooter>
         <IconeComContador
-          // icone={iconeCurtida}
+          icone={iconeCurtida}
           onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
+          valorContador={numeroCurtidas}
         />
 
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={onClickComentario}
-          // valorContador={numeroComentarios}
+          valorContador={numeroComentarios}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {caixaDeComentario}
     </PostContainer>
   )
 }
--- modulo4/use-state/src/components/SecaoComentario/SecaoComentario.js
+++ modulo4/use-state/src/components/SecaoComentario/SecaoComentario.js
@@ -1,35 +1,41 @@
-import React, {useState} from 'react'
-import styled from "styled-components"
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
 const CommentContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 5px;
+  display: flex;
+  justify-content: center;
+  padding: 5px;
 `
 
-const InputComment = styled.input `
-    width: 100%;
-    margin-right: 5px;
+const InputComment = styled.input`
+  width: 100%;
+  margin-right: 5px;
 `
 
-const SecaoComentario = (props) => {
+const SecaoComentario = props => {
+  const [inputValue, setValorInputValue] = useState('')
 
+  const onChangeComentario = event => {
+    setValorInputValue(event.target.value)
+  }
 
-	const onChangeComentario = (event) => {
-	}
-
-	return (
-		<CommentContainer>
-			<InputComment
-				className={'input-comentario'}
-				placeholder={'Comentário'}
-				value={""}
-				onChange={onChangeComentario}
-			/>
-			<button onClick={() => { props.enviarComentario() }} >Enviar</button>
-		</CommentContainer>
-	)
+  return (
+    <CommentContainer>
+      <InputComment
+        className={'input-comentario'}
+        placeholder={'Comentário'}
+        value={inputValue}
+        onChange={onChangeComentario}
+      />
+      <button
+        onClick={() => {
+          props.enviarComentario(inputValue)
+        }}
+      >
+        Enviar
+      </button>
+    </CommentContainer>
+  )
 }
-
 
 export default SecaoComentario
