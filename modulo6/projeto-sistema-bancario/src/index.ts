385e0dfc90bb0eeae7a7e651546facf39eb7f8b1
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-08-27 14:18:51 -0300

    exercício realizado, não foi feito o desafio ainda

--- modulo6/projeto-sistema-bancario/src/index.ts
+++ modulo6/projeto-sistema-bancario/src/index.ts
@@ -1,15 +1,62 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
+import { User, baseUsers } from './data'
+import { validate } from './functions/validateAge'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/bank', (req: Request, res: Response) => {
+let users = baseUsers
+
+app.get('/users', (req: Request, res: Response) => {
   let errorCode = 400
   try {
-    res.status(200).send('Projeto configurado e pronto para ser iniciado')
+    res.status(200).send(users)
+  } catch (error: any) {
+    res.status(errorCode).send(error.message)
+  }
+})
+
+app.post('/users', (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const { cpf, name, birthDate, balance, extract } = req.body
+
+    if (!cpf || !name || !birthDate) {
+      errorCode = 422
+      throw new Error('Ausência de parâmetros no body')
+    }
+
+    const ageValidade = validate(birthDate)
+    if (ageValidade) {
+      users.push({ name, cpf, birthDate, balance: 0, extract: [] })
+    } else {
+      errorCode = 403
+      throw new Error(
+        'Ops, você não tem 18 anos ainda, não é possível criar uma conta'
+      )
+    }
+
+    // let CPF = req.query.cpf as string
+    // const findUserCpf = users.find(user => user.cpf === CPF)
+    // if (findUserCpf) {
+    //   errorCode = 409
+    //   throw new Error('CPF já existente.')
+    // }
+
+    const newUser: User = {
+      cpf,
+      name,
+      birthDate,
+      balance,
+      extract
+    }
+
+    users.push(newUser)
+
+    res.status(200).send({ message: 'Conta criada com sucesso!', users })
   } catch (error: any) {
     res.status(errorCode).send(error.message)
   }
