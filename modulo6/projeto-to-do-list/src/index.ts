c90388d9d4a00e0f8ea1cce218d0517e8c2dfd73
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-09-11 21:34:56 -0300

    Projeto ToDoList concluído

--- modulo6/projeto-to-do-list/src/index.ts
+++ modulo6/projeto-to-do-list/src/index.ts
@@ -8,7 +8,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-
 app.get('/users/:id', async (req: Request, res: Response) => {
   let errorCode = 400
   try {
@@ -64,7 +63,7 @@ app.post('/users', async (req: Request, res: Response) => {
   }
 })
 
-app.put("/users/edit/:id", async (req: Request, res: Response) => {
+app.put('/users/edit/:id', async (req: Request, res: Response) => {
   let errorCode = 400
   try {
     const id = Number(req.params.id)
@@ -76,19 +75,19 @@ app.put("/users/edit/:id", async (req: Request, res: Response) => {
     `)
 
     if (usuarios[0].length === 0) {
-      throw new Error("Usuário não encontrado")
+      throw new Error('Usuário não encontrado')
     }
 
     if (!name) {
-      throw new Error("É necessário informar o novo nome")
+      throw new Error('É necessário informar o novo nome')
     }
 
     if (!nickname) {
-      throw new Error("É necessário informar o novo apelido")
+      throw new Error('É necessário informar o novo apelido')
     }
 
     if (!email) {
-      throw new Error("É necessário informar o novo email")
+      throw new Error('É necessário informar o novo email')
     }
 
     await connection.raw(`
@@ -97,11 +96,10 @@ app.put("/users/edit/:id", async (req: Request, res: Response) => {
     WHERE id = ${id};
   `)
 
-    res.status(200).send("Cadastro alterado")
+    res.status(200).send('Cadastro alterado')
   } catch (error) {
     res.status(errorCode).send(error.message)
   }
-
 })
 
 app.get('/task/:id', async (req: Request, res: Response) => {
@@ -112,7 +110,7 @@ app.get('/task/:id', async (req: Request, res: Response) => {
     if (id) {
       const result = await connection.raw(`
         SELECT * FROM ToDoList
-        WHERE id = "${id}";
+        WHERE id = ${id};
       `)
 
       return res.status(200).send(result[0])
@@ -134,7 +132,9 @@ app.post('/task', async (req: Request, res: Response) => {
     const { title, description, limitDate, status, creatorUserId } = req.body
 
     if (!title || !description || !limitDate || !status || !creatorUserId) {
-      throw new Error('Passe o título, a descrição, a data limite, o status e o id do usuário')
+      throw new Error(
+        'Passe o título, a descrição, a data limite, o status e o id do usuário'
+      )
     }
 
     const toDoList = await connection.raw(`
@@ -224,6 +224,164 @@ app.get('/user', async (req: Request, res: Response) => {
   }
 })
 
+app.get('/task/:id/responsible', async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const id = Number(req.params.id)
+
+    if (id) {
+      const result = await connection.raw(`
+        SELECT t.id as taskId,  u.nickname as Nickname
+        FROM ToDoList as t
+        INNER JOIN Users as u ON t.creatorUserId = u.id
+        WHERE t.id = ${id}
+      `)
+
+      return res.status(200).send(result[0])
+    }
+
+    const result = await connection.raw(`
+      SELECT * FROM ToDoList;
+    `)
+
+    res.status(200).send(result[0])
+  } catch (error) {
+    res.status(errorCode).send(error.message)
+  }
+})
+
+app.get('/task/:id/users', async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const id = Number(req.params.id)
+
+    if (id) {
+      const result = await connection.raw(`
+        SELECT t.id as taskId, t.title, t.description, t.limitDate, t.creatorUserId, u.nickname as creatorUserNickname
+        FROM ToDoList as t
+        INNER JOIN Users as u ON t.creatorUserId = u.id
+        WHERE t.id = ${id}
+      `)
+
+      return res.status(200).send(result[0])
+    }
+
+    const result = await connection.raw(`
+      SELECT * FROM ToDoList;
+    `)
+
+    res.status(200).send(result[0])
+  } catch (error) {
+    res.status(errorCode).send(error.message)
+  }
+})
+
+app.put('/task/status/:id', async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const id = Number(req.params.id)
+    const { status } = req.body
+
+    const tasks = await connection.raw(`
+      SELECT status FROM ToDoList
+      WHERE id = ${id};
+    `)
+
+    if (tasks[0].length === 0) {
+      throw new Error('Tarefa não encontrada')
+    }
+
+    if (!status) {
+      throw new Error('É necessário informar o novo status da tarefa')
+    }
+
+    await connection.raw(`
+    UPDATE ToDoList
+    SET status = "${status}" 
+    WHERE id = ${id};
+  `)
+
+    res.status(200).send('Cadastro alterado')
+  } catch (error) {
+    res.status(errorCode).send(error.message)
+  }
+})
+
+app.get('/tasks', async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const status = req.query.status
+
+    if (status) {
+      const result = await connection.raw(`
+        SELECT t.id as taskId, t.title, t.description, t.limitDate, t.creatorUserId, u.nickname as creatorUserNickname
+        FROM ToDoList as t
+        INNER JOIN Users as u ON t.creatorUserId = u.id
+        WHERE t.status = "${status}";
+      `)
+
+      res.status(200).send(result[0])
+    }
+
+    const result = await connection.raw(`
+      SELECT * FROM ToDoList;
+    `)
+
+    res.status(200).send(result[0])
+  } catch (error) {
+    res.status(errorCode).send(error.message)
+  }
+})
+
+app.delete("/task/:id", async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const id = Number(req.params.id)
+
+    const task = await connection.raw(`
+      SELECT * FROM ToDoList
+      WHERE id = ${id};
+    `)
+
+    if (task[0].length === 0) {
+      throw new Error("Tarefa não encontrado")
+    }
+
+    await connection.raw(`
+      DELETE FROM ToDoList
+      WHERE id = ${id};
+    `)
+
+    res.status(200).send("Tarefa deletado")
+  } catch (error) {
+    res.status(errorCode).send(error.message)
+  }
+})
+
+app.delete("/user/:id", async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const id = Number(req.params.id)
+
+    const user = await connection.raw(`
+      SELECT * FROM Users
+      WHERE id = ${id};
+    `)
+
+    if (user[0].length === 0) {
+      throw new Error("Usuário não encontrado")
+    }
+
+    await connection.raw(`
+      DELETE FROM Users
+      WHERE id = ${id};
+    `)
+
+    res.status(200).send("Usuário deletado")
+  } catch (error) {
+    res.status(errorCode).send(error.message)
+  }
+})
 
 app.listen(process.env.PORT || 3003, () => {
   console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
